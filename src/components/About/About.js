7a3eacfb09b6653d6559e5b11a968ffbc784b751
Jose Mejia <mejialaguna@yahoo.com>
2021-12-13 20:03:15 -0500

    changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,36 +19,24 @@ const About = () => {
 
       <div className='about__contact center'>
         {resume && (
-          <a href={resume} style={{display: "block"}}>
+          <a href={resume} style={{ display: 'block' }}>
             <span type='button' className='btn btn--outline'>
               Resume
             </span>
           </a>
         )}
 
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                <GitHubIcon />
-              </a>
-            )}
+        <a href={social.github} aria-label='github' className='link link--icon'>
+          <GitHubIcon />
+        </a>
 
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                <LinkedInIcon />
-              </a>
-            )}
-          </>
-        )}
+        <a
+          href={social.linkedin}
+          aria-label='linkedin'
+          className='link link--icon'
+        >
+          <LinkedInIcon />
+        </a>
       </div>
     </div>
   )
